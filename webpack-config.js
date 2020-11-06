352ad9b5d0b7f9062dbfec48f73e7a1aeba578e0
coleus99 <christopherjohncoles@gmail.com>
2020-11-06 20:23:06 +0000

    reverted output path back to dist folder

--- webpack-config.js
+++ webpack-config.js
@@ -7,7 +7,6 @@ module.exports = {
   mode: 'development',
   output: {
     filename: 'main.js',
-    // path: path.resolve(__dirname, 'dist'),
-    path: '/',
+    path: path.resolve(__dirname, 'dist'),
   },
 };
